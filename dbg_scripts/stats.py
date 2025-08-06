a97abe40b552f4359f213a9a5d1a5aadeb5eaa85
XZ-X <xu1415@purdue.edu>
2025-08-06 01:23:57 -0400

    rename vars

---
+++ dbg_scripts/stats.py
@@ -0,0 +1,10 @@
+import json
+from tqdm import tqdm
+
+
+data_in = [json.loads(line) for line in tqdm(open("data_out/syn_sec_code_tasks.jsonl", "r"))]
+
+total = 0
+for item in data_in:
+    total += len(item['succ_tasks'])
+print(len(data_in))
